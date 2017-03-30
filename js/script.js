f7628e7dc9707af3fcee31ab4b9a9d7ac1199e2f
BrandonHAnderson <BrandonHAnderson@gmail.com>
2017-03-29 19:03:25 -0500

    testing mime

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+console.log("hello js");
+$(function () {
+    'use strict';
+    console.log("Weather App");
+
+    $.ajaxSetup({
+        dataType: "jsonp"
+    });
+    $.ajax({
+        url: "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us,units=Imperial&appid=b1b15e88fa797225412429c1c50c122a1",
+        dataType: "jsonp",
+        contentType: "applications/javascript",
+        success: function (data) {
+            console.log(data);
+
+        }
+
+    });
+
+
 });
+
+
+// My Key 1facaaab905e18b5369e1dfd1605da85
