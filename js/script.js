191445f53907ecf2c2d56daf2514a4f00bfba915
nguyhai <tenth11@gmail.com>
2017-04-03 23:36:19 -0500

    cleaned it up a bit

--- js/script.js
+++ js/script.js
@@ -2,30 +2,10 @@ console.log("hello js");
 $(function () {
  
 console.log("hello");
- 
-/*
-   getFeaturesHtml = function (coord) {
-       
-        //Display the features
-        html = "";
-        html += "<li>" + coord.lat + " <br> " + coord.lon +"</li>";
-        return html;
-    }   
- 
-*/
    
     displayDescription = function (target, weather) {
         html = "";
         html += "<ul><h2>Weather Description</h2>";
- 
-        /*
-        $.each(coordArray, function (index) {
-       
-            console.log(coordArray[index]);
-            coord = coordArray[index];
-            html += getFeaturesHtml(coord);
-        })
-        */
         html += "<li>" + weather["0"].description + "</li>";
         html += "<li>" + weather["1"].description + "</li>";
        
@@ -33,21 +13,16 @@ console.log("hello");
         $(target).append(html);
     }  
     
-        currentTemp = function (target, main) {
+    currentTemp = function (target, main) {
         html = "";
         html += "<ul><h2>Temperature</h2>";
- 
         html += "<li>" + main.temp + 'F' +"</li>";
-
         html += "</ul>";
         $(target).append(html);
     }  
 
-    //Clicking on the submit button
 $("#submit").on("click", function(){
    
-    
-    //Declare variables for the values on the form
     let zipcode = $("#zip").val();
     let weatherApiKey = $("#apikey").val();
     let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + weatherApiKey + "&units=imperial";
@@ -57,14 +32,9 @@ $("#submit").on("click", function(){
    $.getJSON(url, function (data) {
  
     console.log("1", data);
-    //For assignment build HTML to show info on the web page
-   
- 
-     displayDescription("#weather", data.weather);
-     currentTemp("#weather", data.main);     
-        
-        
-    
+    displayDescription("#weather", data.weather);
+    currentTemp("#weather", data.main);     
+
     }).fail(function (data) {
         console.log(data);
     });
