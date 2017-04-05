1e607f30b1de9f32fc6260d435620bec769affac
Kenner Luoma <kennerluoma@gmail.com>
2017-04-05 17:45:29 -0500

    My simple weather app

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+$(function() {
+  const $submit = $(`#submit`),
+    $weather = $(`#weather`);
+  $submit.click(function() {
+    let key = $(`#apikey`).val(),
+      zip = $(`#zip`).val(),
+      country = `us`,
+      url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${key}&units=imperial`;
+    $.getJSON(url, function(data) {
+      let temp = Math.floor(data.main.temp),
+        description = data.weather[0].description,
+        city = data.name;
+      $weather.html(`<p>The weather is ${description} and ${temp}&deg;F in ${city}.</p>`);
+      console.log(data.weather);
+    }).fail(function(data) {
+      console.log(`failed ` + data);
+    });
+  });
 });
