c0b464171df4db4b6128ce6306ca48fc4f52648f
Josh Hight <Josh.Hight@chrobinson.com>
2017-04-05 17:28:01 -0500

    Checkin for Inclass
    Worked with Hai Nguyen on both of these (simple and advanced)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
-$(function() {
+console.log("hello js"); 
+$(function () { 
+  
+console.log("hello"); 
+    
+    displayDescription = function (target, weather) { 
+        html = ""; 
+        html += "<ul><h2>Weather Description</h2>"; 
+        html += "<li>" + weather["0"].description + "</li>"; 
+         html += "<li>" + weather["1"].description + "</li>"; 
+         
+         html += "</ul>"; 
+         $(target).append(html); 
+     }   
+      
+     currentTemp = function (target, main) { 
+         html = ""; 
+         html += "<ul><h2>Temperature</h2>"; 
+         html += "<li>" + main.temp + 'F' +"</li>"; 
+         html += "</ul>"; 
+         $(target).append(html); 
+     }   
+ 
 
-   console.log("hello");
+ $("#submit").on("click", function(){ 
+     
+     let zipcode = $("#zip").val(); 
+     let weatherApiKey = $("#apikey").val(); 
+     let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + weatherApiKey + "&units=imperial"; 
+     console.log("The API Key and Zip Code are: " + weatherApiKey + " and", + zipcode ); 
+     console.log("The url for this is: " + url ); 
+   
+    $.getJSON(url, function (data) { 
+   
+     console.log("1", data); 
+     displayDescription("#weather", data.weather); 
+     currentTemp("#weather", data.main);      
+ 
 
-
-});
+     }).fail(function (data) { 
+         console.log(data); 
+     }); 
+     
+ }) 
+      
+ }); 
